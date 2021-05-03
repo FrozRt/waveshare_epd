fbef1421e17b36bb11a9b482ca8ead95c16a6538
Mikhail Chigrin <chmv@liis.su>
2021-05-03 11:52:12 +0300

    Reformat code

---
+++ core/epd_5in83.py
@@ -0,0 +1,39 @@
+import sys
+import os
+
+picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
+libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
+if os.path.exists(libdir):
+    sys.path.append(libdir)
+
+import logging
+from waveshare_epd import epd5in83
+import time
+from PIL import Image, ImageDraw, ImageFont
+import traceback
+
+try:
+    # Display init, clear
+    display = epd5in83.EPD()
+    display.init()
+    display.Clear()  # 0: Black, 255: White
+    h = display.height
+    w = display.width
+
+    font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
+    image = Image.new('1', (display.height, display.width), 255)
+    draw = ImageDraw.Draw(image)
+    draw.text((0, 0), "Let's check it", font=font24, fill=0, align='left')
+    display.display(display.getbuffer(image))
+    display.Clear()
+
+    # Show Thanos image.
+    time.sleep(3)  # Pause for 3 seconds.
+    thanos = Image.open(os.path.join(picdir, '5in65_n5.bmp'))
+    image.paste(thanos, (0, 0))
+    display.display(display.getbuffer(image))  # Update display
+    time.sleep(10)
+    display.Clear()
+
+except IOError as e:
+    print(e)
--- lib/waveshare_epd/epdconfig.py
+++ lib/waveshare_epd/epdconfig.py
@@ -1,32 +1,3 @@
-# /*****************************************************************************
-# * | File        :	  epdconfig.py
-# * | Author      :   Waveshare team
-# * | Function    :   Hardware underlying interface
-# * | Info        :
-# *----------------
-# * | This version:   V1.0
-# * | Date        :   2019-06-21
-# * | Info        :   
-# ******************************************************************************
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documnetation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to  whom the Software is
-# furished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS OR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
-#
-
 import os
 import logging
 import sys
@@ -35,10 +6,10 @@ import time
 
 class RaspberryPi:
     # Pin definition
-    RST_PIN         = 17
-    DC_PIN          = 25
-    CS_PIN          = 8
-    BUSY_PIN        = 24
+    RST_PIN = 17
+    DC_PIN = 25
+    CS_PIN = 8
+    BUSY_PIN = 24
 
     def __init__(self):
         import spidev
@@ -89,10 +60,10 @@ class RaspberryPi:
 
 class JetsonNano:
     # Pin definition
-    RST_PIN         = 17
-    DC_PIN          = 25
-    CS_PIN          = 8
-    BUSY_PIN        = 24
+    RST_PIN = 17
+    DC_PIN = 25
+    CS_PIN = 8
+    BUSY_PIN = 24
 
     def __init__(self):
         import ctypes
@@ -153,6 +124,3 @@ else:
 
 for func in [x for x in dir(implementation) if not x.startswith('_')]:
     setattr(sys.modules[__name__], func, getattr(implementation, func))
-
-
-### END OF FILE ###
