44a4b345ebaa1f054fba7ed136dabe9381e178c0
Mikhail Chigrin <chmv@liis.su>
2021-05-03 12:27:05 +0300

    Add function

--- core/epd.py
+++ core/epd.py
@@ -1,50 +1,56 @@
-import sys
 import os
+import sys
+import logging
+import time
+from PIL import Image, ImageDraw, ImageFont
+
+from waveshare_epd import epd5in83
 
 picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
 libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
 if os.path.exists(libdir):
     sys.path.append(libdir)
 
-import logging
-from waveshare_epd import epd5in83
-import time
-from PIL import Image, ImageDraw, ImageFont
-
 logging.basicConfig(level=logging.DEBUG)
 
-try:
-    logging.info("epd5in83")
 
-    # Display init, clear
-    logging.info("init and clear")
-    display = epd5in83.EPD()
-    display.init()
-    display.Clear()  # 0: Black, 255: White
+def epd_run(pic_path: str):
+    try:
+        logging.info("epd5in83")
 
-    logging.info("drawing on the horizontal image")
-    font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
-    image = Image.new('1', (display.width, display.height), 255)
-    draw = ImageDraw.Draw(image)
-    draw.text((0, 0), "Let's check it", font=font24, fill=0, align='left')
-    display.display(display.getbuffer(image))
-    display.Clear()
+        # Display init, clear
+        logging.info("init and clear")
+        display = epd5in83.EPD()
+        display.init()
+        display.Clear()  # 0: Black, 255: White
 
-    # Show Thanos image.
-    logging.info("show image during the 10 secs")
-    time.sleep(3)  # Pause for 3 seconds.
-    thanos = Image.open(os.path.join(picdir, '5in65_n5.bmp'))
-    image.paste(thanos, (0, 0))
-    display.display(display.getbuffer(image))  # Update display
-    time.sleep(10)
+        logging.info("drawing on the horizontal image")
+        font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
+        image = Image.new('1', (display.width, display.height), 255)
+        draw = ImageDraw.Draw(image)
+        draw.text((0, 0), "Let's check it", font=font24, fill=0, align='left')
+        display.display(display.getbuffer(image))
+        display.Clear()
 
-    logging.info("clear display")
-    display.Clear()
+        # Show Thanos image.
+        logging.info("show image during the 10 secs")
+        thanos = Image.open(pic_path)
+        image.paste(thanos, (0, 0))
+        display.display(display.getbuffer(image))  # Update display
+        time.sleep(10)
 
-except IOError as e:
-    print(e)
+        logging.info("clear display")
+        display.Clear()
 
-except KeyboardInterrupt:
-    logging.info("ctrl + c:")
-    epd5in83.epdconfig.module_exit()
-    exit()
+    except IOError as e:
+        print(e)
+
+    except KeyboardInterrupt:
+        logging.info("ctrl + c:")
+        epd5in83.epdconfig.module_exit()
+        exit()
+
+
+if __name__ == '__main__':
+    test_path = os.path.join(picdir, '5in65_n5.bmp')
+    epd_run(test_path)
